ea5f4c461f5e14ce86fcee7262e961dc8430fb55
Sean Tracey <seanmtracey@gmail.com>
2016-07-07 15:45:42 +0100

    Automate creation of directorys for fake S3 service

--- scan-the-slices/main.js
+++ scan-the-slices/main.js
@@ -6,12 +6,24 @@ var debug = require('debug')('scan-the-slices:main.js');
 if(process.env.ENVIRONMENT === 'DEVELOPMENT'){
 		// We're going to fire up a fake S3 server
 		debug("In development environment. Firing up fake S3 server...");
-	
+
+		try{
+			fs.accessSync('./bin/');
+		} catch(err){
+			fs.mkdirSync('./bin/');
+		}
+
+		try{
+			fs.accessSync('./bin/s3');
+		} catch(err){
+			fs.mkdirSync('./bin/s3');
+		}
+
 		const s3Client = new S3rver({
 				port : 7890,
 				hostname: 'localhost',
 				silent: true,
-				directory: '/bin/s3/'
+				directory: './bin/s3/'
 			}).run();
 		;
 

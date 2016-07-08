cbec476be4c71a99ead9074a03204af96d6688c1
Sean Tracey <seanmtracey@gmail.com>
2016-07-08 15:00:04 +0100

    Script should now be able to handle having an S3 key passed to it, retrieve the file, and then OCR it.

--- scan-the-slices/main.js
+++ scan-the-slices/main.js
@@ -2,43 +2,107 @@ require('node-babel')();
 
 const dotenv = require('dotenv').config();
 
-const S3rver = require('s3rver');
-const argv = require('yargs').argv; 
 const debug = require('debug')('scan-the-slices:main.js');
+const AWS = require("aws-sdk");
+const S3rver = require('s3rver');
+const argv = require('yargs').argv;
+const fs = require('fs');
+const validUrl = require('valid-url').isUri;
+var First = undefined;
 
 const tesseract = require('./lib/tesseract');
+
+const S3config = {
+	s3ForcePathStyle: true,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	endpoint: new AWS.Endpoint(process.env.S3_ENDPOINT)
+}
+
+const S3 = new AWS.S3(S3config);
 
 if(process.env.ENVIRONMENT === 'DEVELOPMENT'){
 	
 	// We're going to fire up a fake S3 server
 	debug("In development environment. Firing up fake S3 server...");
 
-	const S3Server = new S3rver({
-		port : 7890,
-		hostname: 'localhost',
-		silent: true,
-		directory: './bin/s3/'
-	}).run();
+	First = new Promise( (resolve, reject) => {
 
+		const S3Server = new S3rver({
+			port : 7890,
+			hostname: 'localhost',
+			silent: true,
+			directory: './bin/s3/'
+		}).run();
+
+		S3.createBucket({
+			Bucket : 'articles'
+		}, function(){
+
+			S3.upload({
+				Bucket : 'articles',
+				Key : 'FTDA-1940-0706-0002-003',			
+				Body : fs.readFileSync('./resources/FTDA-1940-0706-0002-003.jpg')
+			}, function(){
+				resolve();
+				console.log('Test file successfully uploaded to fake S3 bucket');
+			});
+
+		});
+
+	});
+
+} else {
+	First = new Promise.resolve();
 }
 
-const sourceDocument = argv.src;
+const resourceID = argv.src;
 
-debug(sourceDocument);
+function scan(doc){
 
-if(sourceDocument !== undefined){
-	// ./bin/s3/FTDA-1940-0706-0002-003.jpg
-	tesseract(sourceDocument, true)
+	return tesseract(doc, true)
 		.then(res => {
-			console.log(res);
-
+			// console.log(res);
+			// We should process the result here.
 			const formattedText = res[0];
 			const boundedText = res[1];
+
+			return res;
 
 		})
 		.catch(err => {
 			console.log(err);
 		})
 	;
+
+}
+
+if(resourceID !== undefined){
+	// FTDA-1940-0706-0002-003
+	// Go and get the image from the URL, store it locally, and then pass it to tesseract
+
+	First.then(function(){
+		const destination = `./bin/tmp/in/${resourceID}.jpg`;
+		const file = fs.createWriteStream(destination);
+		S3.getObject({
+			Bucket : 'articles',
+			Key : resourceID
+		}).createReadStream().pipe(file);
+
+		file.on('error', function(e){
+			console.log("error event");
+			console.log(e);
+		});
+
+		file.on('close', function(e){
+			console.log(`File recieved from S3 and written to ${destination}`);
+			scan(destination)
+				.then(res => {
+					console.log(res);
+				})
+			;
+		});
+
+	});
 
 }

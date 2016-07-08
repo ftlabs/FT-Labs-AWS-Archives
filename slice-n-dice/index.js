c5365dbef3d0b6fd0a7e4b378ec009deed49965b
Jake Champion <jakechampion.jake2@googlemail.com>
2016-07-08 16:35:06 +0100

    Grab the CWD programatically

--- slice-n-dice/index.js
+++ slice-n-dice/index.js
@@ -3,8 +3,8 @@
 var xmlParser = require('./xml-parser');
 var imageProcessing = require('./image-processing');
 
-var workingDirectory = '/Users/peter.clark/Code/FT-Labs-AWS-Archives/slice-n-dice';
 var directory = '/Users/peter.clark/Documents/1939/19390216/';
+var workingDirectory = require('process').cwd();
 
 xmlParser.run().then(function(data){
   for (i in data.articles){
